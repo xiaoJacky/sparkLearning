3737a85a23c67368c19bcbeb598fce1d4aec911a
xiaoJacky <953757056@qq.com>
2017-07-08 16:43:15 +0800

    DEV:学习breeze库

--- sparkLearning/src/main/scala/com/learn/spark/mllib/LearnBreeze.scala
+++ sparkLearning/src/main/scala/com/learn/spark/mllib/LearnBreeze.scala
@@ -1,12 +1,15 @@
 package com.learn.spark.mllib
 
+import breeze.linalg.DenseVector
+
 /**
-  * Created by Administrator on 2017/7/8 0008.
+  * Created by xiaojie on 2017/7/8 0008.
   */
 object LearnBreeze {
 
   def main(args: Array[String]): Unit = {
-
+    val x = DenseVector.zeros[Double](5)
+    println(x)
   }
 
 }
