bb82266dd685acc8e05fefadf76a5bf0c1b53091
xiaoJacky <953757056@qq.com>
2017-07-26 16:55:13 +0800

    update

--- sparkLearning/src/main/scala/com/learn/spark/mllib/ClassificationConstraint.scala
+++ sparkLearning/src/main/scala/com/learn/spark/mllib/ClassificationConstraint.scala
@@ -44,6 +44,7 @@ object ClassificationConstraint {
      * @param yMatrix
      **/
     def train(xMatrix: DenseMatrix[Double], yMatrix: DenseMatrix[Double]): Unit = {
+
         //x矩阵的转置
         val xMatrixT = xMatrix.t
         val xxMatrixT = inv(xMatrixT * xMatrix).asInstanceOf[DenseMatrix[Double]] //(x矩阵的转置 * x)矩阵的逆
@@ -55,7 +56,7 @@ object ClassificationConstraint {
         val loop = new Breaks
         loop.breakable {
             //迭代训练
-            for(h <- 0 until 100000) {
+            for(h <- 0 until 500000) {
 
                 val xtxMatrix = (xMatrixT * xMatrix).asInstanceOf[DenseMatrix[Double]]
                 val addMatrix = xtxMatrix + wDiagMatrix0
@@ -75,6 +76,19 @@ object ClassificationConstraint {
         }
 
         println(wMatrix)
+
+        val resultMatrix = (xMatrix * wMatrix).asInstanceOf[DenseMatrix[Double]]
+        val m = resultMatrix.rows
+        var lost = 0
+        var right = 0
+        for(i <- 0 until m){
+            val y0 = yMatrix.apply(i, 0)
+            val y1 = resultMatrix.apply(i, 0)
+            if(sign(y1) == y0) right = right + 1 else lost = lost + 1
+        }
+
+        println(s"right=$right, lost=$lost")
+
 
     }
 
@@ -98,5 +112,18 @@ object ClassificationConstraint {
         exp(denseMatrix)
     }
 
+    /**
+     * 阶跃函数
+     * @param x
+     * */
+    def sign(x: Double): Double = {
+        if(x > 0 ){
+            1.0
+        }else if(x == 0){
+            -1
+        }else {
+            0
+        }
+    }
 
 }
